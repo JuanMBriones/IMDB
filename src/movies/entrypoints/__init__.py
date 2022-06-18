30a35c190af2ad0b9ddb022dc781af6d39f96c9b
Juan Briones <JuanMBriones@outlook.com>
2022-06-18 16:48:10 -0500

    Segregate route management and flask app initialization

---
+++ src/movies/entrypoints/__init__.py
@@ -0,0 +1,38 @@
+""""
+TODO
+- Return 10 recommendations from the top 250 movies
+- Generate user preference key
+- User sign-up
+    - Selection of 3 preferences
+- User sign-in
+
+- 3 SOLID principles
+- 2 design patterns
+- Clean architecture
+
+- The project builds and launches successfully.
+- You can consume an endpoint to obtain a recommendation list
+- You can add a parameter to obtain a recommendation in descending order.
+- System design document correctly identify functional and non functional requirements.
+- Use cases diagram expresses the two main functionalities of the system.
+- The sequence diagrams show the steps of both processes fully..
+- The 3 SOLID principles are identified inside the project.
+- The class diagram expresses one design pattern used in the project.
+- The class diagram expresses one design pattern used in the project.
+- The project implements clean architecture fully to separate modules and classes.
+"""
+
+from flask import Flask, request, render_template
+from movies import models
+
+
+def create_app():
+    app = Flask(__name__)
+    models.start_mappers()
+    
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+    #app.run(debug=True)
+app = create_app()
---
+++ src/movies/entrypoints/main.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, Flask, request, render_template
+from .flask_app import models
+import csv, os
+#from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route("/", methods=["GET"])
+def index():
+    return render_template('index.html')
+
+@main.route("/hello", methods=["GET"])
+def hello_world():
+    models.get_movie()
+    return "Hello World!", 200
+
+@main.route("/recommendations", methods=["GET"])
+def recommendations():
+    CSV_PREFERENCE_KEY = 0
+    CSV_MOVIE_TITLE = 1
+
+    args = request.args
+    key = args.get('key', type=int)
+    rating = args.get('rating', default=True, type=lambda v: v.lower() == 'true')
+
+    movies = []
+
+    with open('./static/movie_results.csv') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        next(reader, None)
+
+        movies = [
+            row[CSV_MOVIE_TITLE]
+            for row in reader
+            if int(row[CSV_PREFERENCE_KEY]) == key
+        ]
+
+        if not rating:
+            movies = movies[::-1]
+
+    return render_template('recommendations.html', key=key, movies=movies)
